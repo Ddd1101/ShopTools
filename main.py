54791f927f7f442f4c055caeeb5d87c20fa99e45
raynorhou <raynorhou@tencent.com>
2022-08-14 19:41:53 +0800

    计算改线程不阻塞ui

--- main.py
+++ main.py
@@ -5,14 +5,187 @@ from PySide2.QtCore import QFile, QDate,   QDateTime , QTime
 
 import os
 from datetime import datetime, timedelta
-import Trade as TransactionData
-import Common.utils as utils
 import xlsxwriter
 import io
 import urllib
 import time
 
+import requests
+
+import hmac
+import math
+
+import xlrd
+import xlwt
+import PySide2
+
+import _thread
+
+
+dirname = os.path.dirname(PySide2.__file__)
+plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
+AppKey = {'联球制衣厂':'2460951','朝雄制衣厂':'4048570', '朝逸饰品厂':'7464845'}
+AppSecret =  {"联球制衣厂":b'AnBm2v0I80x0',"朝雄制衣厂":b'vxMiqUGDZ7p',"朝逸饰品厂":b'Oo0hRGjaaPb'}
+access_token = {'联球制衣厂':'b7635cd6-18b8-4b96-b6a5-d2ab04f203ae','朝雄制衣厂':'f7a85f0e-fb90-40b1-9bc6-b66ad3cf120b', '朝逸饰品厂':'37454060-a51a-497d-9866-0d722a1bd5cc'}
+
+en_code = ['s','S','m','M', 'l','L','x','X']
+
+base_url = 'https://gw.open.1688.com/openapi/'
+
+
 path = os.path.realpath(os.curdir)
+
+price_path = path + '/price.xlsx'
+
+request_type = "param2/1/com.alibaba.trade/"
+url = base_url + request_type
+
+def GetPriceGrid():
+    workbook = xlrd.open_workbook(price_path)  # 打开工作簿
+    sheets = workbook.sheet_names()  # 获取工作簿中的所有表格
+    worksheet = workbook.sheet_by_name(sheets[0])  # 获取工作簿中所有表格中的的第一个表格
+    return worksheet
+
+worksheet = GetPriceGrid()
+
+def NumFormate4Print(numStr):
+    res = ""
+    if numStr[0] in en_code:
+        for item in numStr:
+            if item in en_code:
+                res += item
+            else:
+                break
+        fomateSpaceNum = 5 - len(res)
+        for k in range(fomateSpaceNum):
+            res = " " + res
+
+    else:
+        for item in numStr:
+            if item >= '0' and item <= '9':
+                res += item
+            else:
+                if res[0] == '9':
+                    res += " "
+                break
+        res += "cm"
+    return res
+
+def CalSize(sizeDescription):
+    _size = 0
+    for i in range(len(sizeDescription)):
+        if sizeDescription[i] >= '0' and sizeDescription[i] <= '9':
+            _size = _size*10 + int(sizeDescription[i])
+        else:
+            break
+    return _size
+
+def CalPriceLocationENCode(_size):
+    if _size == "":
+        return -1
+
+    if _size[0] == 'S' or _size[0] == 's':
+        return 28
+
+    if _size[0] == 'M' or _size[0] == 'm':
+        return 29
+
+    if _size[0] == 'L' or _size[0] == 'l':
+        return 30
+
+    if _size[0] == 'x' or _size[0] == 'X':
+        if _size[1] == 'L' or _size[1] == 'l':
+            return 31
+        elif _size[1] == 'x' or _size[1] == 'X':
+            if _size[2] == 'L' or _size[2] == 'l':
+                return 32
+            elif _size[2] == 'x' or _size[2] == 'X':
+                return 33
+
+    return -1
+
+def CalPriceLocation(_size):
+    if _size[0] in en_code:
+        return CalPriceLocationENCode(_size)
+    # print(CalSize(_size))
+    theta = (CalSize(_size) - 90)/5
+    _col = 9 + theta
+    # print(_col)
+    return _col
+
+def GetCost(cargoNumber,skuInfosValue, colNum=0):
+    rowIndex = -1
+    for t in range(1, worksheet.nrows):
+        if str(cargoNumber) == str(worksheet.cell(t, colNum).value):
+            rowIndex = t
+            break
+    if rowIndex == -1:
+        print(1)
+        print(cargoNumber + "==========" + worksheet.cell(t, colNum).value)
+        return -1
+    colIndex = CalPriceLocation(skuInfosValue)
+    if colIndex != None:
+        _price = worksheet.cell(rowIndex, int(colIndex)).value
+    else:
+        _price = ''
+    if _price == '':
+        print(rowIndex, colIndex)
+        print(cargoNumber + "==========" + worksheet.cell(t, colNum).value)
+        _price = -1
+    return float(_price)
+
+def GetAdressAndShopName(cargoNumber):
+    rowIndex = -1
+    for t in range(1, worksheet.nrows):
+        if cargoNumber == worksheet.cell(t, 0).value:
+            rowIndex = t
+            break
+    if rowIndex == -1:
+        return ["","",""]
+    productName = worksheet.cell(rowIndex, 1).value
+    adress = worksheet.cell(rowIndex, 2).value
+    shopName = worksheet.cell(rowIndex, 3).value
+    return [productName,adress,shopName]
+
+def CalPageNum(totalRecord):
+    return math.ceil(totalRecord/20)
+
+def CalculateSignature(urlPath,data, shopName):
+    # 构造签名因子：urlPath
+
+    # 构造签名因子：拼装参数
+    params = list()
+    for key in data.keys():
+        params.append(key+data[key])
+    params.sort()
+    sortedParams = params
+    assembedParams = str()
+    for param in sortedParams:
+        assembedParams = assembedParams+param
+
+    # 合并签名因子
+    mergedParams = urlPath + assembedParams
+    mergedParams = bytes(mergedParams, 'utf8')
+
+    # 执行hmac_sha1算法 && 转为16进制
+    hex_res1 = hmac.new(AppSecret[shopName], mergedParams, digestmod="sha1").hexdigest()
+
+    # 转为全大写字符
+    hex_res1 = hex_res1.upper()
+
+    return hex_res1
+
+# 交易数据获取  默认1页
+def GetTradeData(data, shopName):
+    data['access_token'] = access_token[shopName]
+    _aop_signature = CalculateSignature(request_type + "alibaba.trade.getSellerOrderList/" + AppKey[shopName], data, shopName)
+    data['_aop_signature'] = _aop_signature
+    url = base_url + request_type + "alibaba.trade.getSellerOrderList/" + AppKey[shopName]
+    response = requests.post(url, data=data)
+
+    return response.json()
 
 class Window:
 
@@ -74,15 +247,15 @@ class Window:
 
         createEndTime = datetime(int(endYear), int(endMonth), int(endDay)).strftime('%Y%m%d') + '000000000+0800'
 
-        self.LogOut(str(shopId))
-        self.LogOut(shopName)
-        self.LogOut(str(mode))
-        self.LogOut(createStartTime)
-        self.LogOut(createEndTime)
+        self.LogOut("店铺名 ：" + shopName)
+        self.LogOut("色标 ：" + self.ui.Tag.currentText())
+        self.LogOut("订单开始时间 ：" + self.ui.startTime.date().toString("yyyy-MM-dd"))
+        self.LogOut("订单截止时间 ：" + self.ui.endTime.date().toString("yyyy-MM-dd"))
 
-        self.OrderList(shopId, shopName, int(mode), createStartTime, createEndTime)
-
-        self.LogOut("# 统计完成")
+        try:
+            _thread.start_new_thread(self.OrderList, (shopId, shopName, int(mode), createStartTime, createEndTime,))
+        except:
+            print("Error: 无法启动线程")
 
     def LogOut(self, text):
         self.ui.output.append(text)
@@ -92,8 +265,10 @@ class Window:
         if mode == 0:
             self.GetOrderBill(createStartTime, createEndTime, 'waitsellersend', shopName=shopName)
         else:
-            # GetOrderBill(createStartTime,createEndTime,  'waitsellersend', shopName, mode)
             self.GetOrderBill(createStartTime, createEndTime, 'waitsellersend,waitbuyerreceive', shopName, mode)
+
+        self.LogOut("# 统计完成 \n")
+        self.LogOut("###############################################################")
 
     def GetOrderBill(self, createStartTime, createEndTime, orderstatusStr, shopName, mode = 0):
         orderList = []
@@ -103,9 +278,13 @@ class Window:
         for orderstatus in orderstatusList:
             data = {'createStartTime': createStartTime, 'createEndTime': createEndTime, 'orderStatus': orderstatus,
                     'needMemoInfo': 'true'}
-            response = TransactionData.GetTradeData(data, shopName)
-            self.LogOut('# ' + orderstatus + ' : ' + str(response['totalRecord']) + '条记录')
-            pageNum = utils.CalPageNum(response['totalRecord'])
+            response = GetTradeData(data, shopName)
+            if orderstatus == 'waitsellersend' :
+                orderstatusStr = '待发货'
+            if orderstatus == 'waitbuyerreceive' :
+                orderstatusStr = '已发货'
+            self.LogOut('# ' + orderstatusStr + ' : ' + str(response['totalRecord']) + '条记录')
+            pageNum = CalPageNum(response['totalRecord'])
 
             BeihuoJson = {}
 
@@ -113,7 +292,7 @@ class Window:
             for pageId in range(pageNum):
                 data = {'page': str(pageId + 1), 'createStartTime': createStartTime, 'createEndTime': createEndTime,
                         'orderStatus': orderstatus, 'needMemoInfo': 'true'}
-                response = TransactionData.GetTradeData(data, shopName)
+                response = GetTradeData(data, shopName)
 
                 if orderstatus == 'waitsellersend':
                     for order in response['result']:
@@ -165,7 +344,7 @@ class Window:
                         BeihuoJson[cargoNumber][color]['products'][height]['quantity'] = BeihuoJson[cargoNumber][color]['products'][height]['quantity'] + productItem['quantity']
 
                     # 单价
-                    BeihuoJson[cargoNumber][color]['products'][height]['price'] = utils.GetCost(cargoNumber, height)
+                    BeihuoJson[cargoNumber][color]['products'][height]['price'] = GetCost(cargoNumber, height)
 
                     # 总价
                     BeihuoJson[cargoNumber][color]['products'][height]['cost'] = BeihuoJson[cargoNumber][color]['products'][height]['price']*BeihuoJson[cargoNumber][color]['products'][height]['quantity']
@@ -176,7 +355,7 @@ class Window:
         BeihuoTable = []
         for item in BeihuoJson:
             BeihuoList.append(item)
-            adressAndShopName = utils.GetAdressAndShopName(item)
+            adressAndShopName = GetAdressAndShopName(item)
             BeihuoList.append(adressAndShopName[2])
             BeihuoList.append(adressAndShopName[1])
             BeihuoList.append(adressAndShopName[0])
@@ -236,7 +415,7 @@ class Window:
             for height in _list[6]:
                 if amount != "":
                     amount += '\n'
-                amount = amount + utils.NumFormate4Print(height[0]) + ' ' + str(height[1]) + '件'
+                amount = amount + NumFormate4Print(height[0]) + ' ' + str(height[1]) + '件'
 
             BH_sheet.write(BH_x, BH_y, amount)
             BH_y += 1
